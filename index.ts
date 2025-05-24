c54a9f6c59b599694e89f5ecc4c38e3193411732
Hyan Ventura <hyanventura12@hotmail.com>
2025-05-23 23:40:08 -0300

    feat: set cors into project

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config'; 
 import express from "express"
 import { filmeRouter } from "./src/interface/routes/filmeRoutes"
+import cors from "cors"
 
 
 // Create an Express application
@@ -8,6 +9,7 @@ const app = express()
 const port = process.env.PORT || 5000
 
 // Middleware
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
