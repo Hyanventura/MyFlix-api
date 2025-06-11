907fca27a171a3c8358ae3369806f26e3c2f96b9
Hyan Ventura <hyanventura12@hotmail.com>
2025-06-11 18:59:02 -0300

    feat: add type on routes

--- src/interface/routes/filmeRoutes.ts
+++ src/interface/routes/filmeRoutes.ts
@@ -1,14 +1,14 @@
-import {Router} from 'express'
+import { Router, type RequestHandler } from 'express'
 import { createFilmeController, deleteFilmeController, getFilmesController, updateFilmeController } from '../../controllers/filmeController'
 
 
 export const filmeRouter = () => {
     const router = Router()
 
-    router.get('/', getFilmesController)
-    router.post('/', createFilmeController)
-    router.put('/:id', updateFilmeController)
-    router.delete('/:id', deleteFilmeController)
+    router.get('/', getFilmesController as RequestHandler) 
+    router.post('/', createFilmeController as RequestHandler)
+    router.put('/:id', updateFilmeController as RequestHandler)
+    router.delete('/:id', deleteFilmeController as RequestHandler)
 
     return router
 }
