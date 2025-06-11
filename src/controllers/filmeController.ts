c43398c454f26ffff8b56f975794da1fd6bfc580
Hyan Ventura <hyanventura12@hotmail.com>
2025-06-11 19:04:52 -0300

    feat: add comments on controllers

--- src/controllers/filmeController.ts
+++ src/controllers/filmeController.ts
@@ -5,6 +5,9 @@ import pool from '../config/database';
 
 export const getFilmesController = async (req: Request, res: Response) => {
   try {
+
+    // Buscar todos os filmes do banco de dados
+    // e retornar como resposta JSON
     const result = await pool.query('SELECT * FROM filmes');
     res.status(200).json(result.rows);
   } catch (error) {
@@ -20,7 +23,8 @@ export const createFilmeController = async (req: Request, res: Response) => {
     if (foto && !foto.startsWith('data:image/')) {
       return res.status(400).json({ error: "Formato de imagem inválido" });
     }
-    
+    // Inserir um novo filme no banco de dados
+    // e retornar o filme criado como resposta JSON
     const result = await pool.query(
       'INSERT INTO filmes (nome, nota, ano, foto) VALUES ($1, $2, $3, $4) RETURNING *',
       [nome, nota, ano, foto]
@@ -36,6 +40,9 @@ export const updateFilmeController = async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
     const { nome, nota, ano, foto } = req.body;
+
+    // Update um filme existente no banco de dados
+    // e retornar o filme atualizado como resposta JSON
     const result = await pool.query(
       'UPDATE filmes SET nome = $1, nota = $2, ano = $3, foto = $4 WHERE id = $5 RETURNING *',
       [nome, nota, ano, foto, id]
@@ -50,6 +57,9 @@ export const updateFilmeController = async (req: Request, res: Response) => {
 export const deleteFilmeController = async (req: Request, res: Response) => {
   try { 
     const { id } = req.params;
+
+    // Excluir um filme do banco de dados
+    // e retornar um status 204 No Content
     await pool.query('DELETE FROM filmes WHERE id = $1', [id]);
     res.status(204).send();
   } catch (error) {
